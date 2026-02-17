dc57304c766f6dc01ccc68ef9a3adbee281068bb
Dariusz Okoński <darek200180@gmail.com>
2026-02-17 07:21:32 +0100

    add background color to App

--- App.js
+++ App.js
@@ -3,15 +3,15 @@ import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.rootScreen}>
       <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: '#ddb52f',
   },
 });
