2a0ccc5408cfc784fcbeb52bfe6012dc546764e5
Dariusz Okoński <darek200180@gmail.com>
2026-02-18 07:27:40 +0100

    add basic GameScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,31 @@
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const GameScreen = () => {
-  return <Text>GameScreen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>GameScreen</Text>
+
+      {/* GUESS */}
+
+      <View>
+        <Text>Higher or lower?</Text>
+
+        {/** + - */}
+      </View>
+
+      <View>
+        <Text>LOG ROUNDS</Text>
+      </View>
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+    backgroundColor: 'yellow',
+  },
+});
