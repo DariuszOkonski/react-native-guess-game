2fe45585181d2cb3910ecbd0ad6d03778ffcc7d8
Dariusz Okoński <darek200180@gmail.com>
2026-03-03 07:48:22 +0100

    use Platform for Title

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, Platform } from 'react-native';
 import Colors from '../../constants/colors';
 
 const Title = ({ children }) => {
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: Colors.white,
     textAlign: 'center',
-    borderWidth: 2,
+    borderWidth: Platform.select({ ios: 0, android: 2 }),
     borderColor: Colors.white,
     padding: 12,
     maxWidth: '80%',
