ca0ec0a138ffb97ab9063f4b9cb707a45e472fc8
Dariusz Okoński <darek200180@gmail.com>
2026-03-24 09:09:34 +0100

    working on styling

--- App.js
+++ App.js
@@ -4,11 +4,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another piece of text!</Text>
+        <Text style={styles.dummyText}>Another piece of text!</Text>
       </View>
-      <Text style={{ margin: 16, borderWidth: 2, borderColor: '#ff0000' }}>
-        Hello World!!!
-      </Text>
+      <Text style={styles.dummyText}>Hello World!!!</Text>
       <Button title='Tap me!' />
     </View>
   );
@@ -20,5 +18,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: 'blue',
   },
 });
