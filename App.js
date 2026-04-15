61c6cddf8dcb0d7ef6e813d73b50e3c28d119174
Dariusz Okoński <darek200180@gmail.com>
2026-04-15 08:59:33 +0200

    style layout

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
         <Button title='Add Goal' />
       </View>
 
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -18,19 +18,26 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    padding: 50,
-    backgroundColor: 'yellow',
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    // backgroundColor: 'green',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
