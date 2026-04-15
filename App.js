994f4805463dbd53d7e064e93df467c339e86662
Dariusz Okoński <darek200180@gmail.com>
2026-04-15 08:27:27 +0200

    add basic layout

--- App.js
+++ App.js
@@ -1,30 +1,24 @@
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       <View>
-        <Text style={styles.dummyText}>Another piece of text!</Text>
+        <TextInput placeholder='Your course goal!' />
+        <Button title='Add Goal' />
       </View>
 
-      <Text style={styles.dummyText}>Hello World!</Text>
-
-      <Button title='Tap me!!!' />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 1,
-    borderColor: 'red',
+    padding: 50,
+    backgroundColor: 'yellow',
   },
 });
